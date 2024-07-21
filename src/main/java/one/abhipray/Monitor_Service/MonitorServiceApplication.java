2832bf8ad8f2a2c3f36babd32fa0a8edc44f03c0
abhipray-cpu <dumkaabhipray@gmail.com>
2024-07-21 19:12:08 +0530

    monitor-service

---
+++ src/main/java/one/abhipray/Monitor_Service/MonitorServiceApplication.java
@@ -0,0 +1,13 @@
+package one.abhipray.Monitor_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/abhipray/Monitor_Service/MonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package one.abhipray.Monitor_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
